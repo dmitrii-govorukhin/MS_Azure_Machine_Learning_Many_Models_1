1829507ec564b58caf4615ac1d9fc1aa3a26c019
Dmitrii Govorukhin <g06bog.d@gmail.com>
2026-08-19 14:12:40 -0700

    Implement output path argument and save DataFrame
    Added argument parsing for output directory and updated run function to save DataFrame as a Parquet file.

--- many-models-data-generation/src/entry_script.py
+++ many-models-data-generation/src/entry_script.py
@@ -1,23 +1,38 @@
 import pandas as pd
 import itertools
 import csv
+import argparse
+import os
+import uuid
 
-# Headings for the final generated data
-HEADERS = [***]
+
+HEADERS = ["*** hidden code ***"] 
+OUTPUT_DIR = None
+
 
 def init():
-    pass
+    global OUTPUT_DIR
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output_path", type=str)
+    args, _ = parser.parse_known_args()
+    OUTPUT_DIR = args.output_path
 
 
 def generate_rows(source):
-    # Sorry, but the generate function content is a private property.
+    # *** hidden code ***
     
     return rows
-
+    
 
 def run(mini_batch: pd.DataFrame):
     all_rows = []
     for _, row in mini_batch.iterrows():
         data = generate_rows(row.tolist())
         all_rows.extend(data)
-    return pd.DataFrame(all_rows, columns=HEADERS)
+
+    df = pd.DataFrame(all_rows, columns=HEADERS)
+
+    filename = f"part-{uuid.uuid4().hex}.parquet"
+    df.to_parquet(os.path.join(OUTPUT_DIR, filename), index=False, compression="snappy")
+
+    return pd.DataFrame({"rows_written": [len(df)]})
